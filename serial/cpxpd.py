392b518e108414859f1f104f8b4e485ada7b149b
boe <boe@wengle.se>
2022-09-18 15:15:11 +0200

    OK: using imported mhdTracker; TBD: plot power vs time

--- serial/cpxpd.py
+++ serial/cpxpd.py
@@ -6,6 +6,7 @@ import struct
 import matplotlib.pyplot as plt
 import pandas as pd
 from pathlib import Path
+import mhdTracker as MT
 
 formatString = "%Y-%m-%d %H:%M:%S"
 # 2022-09-11 12:12:01
@@ -32,87 +33,22 @@ if True:
         with open('cpxpd_log.txt', 'a') as logfile:
             print(msg, file=logfile)
 
-    def mPlot():
-        tmpPath = Path('../test/tmp_mplot.svg')
-        realPath = Path('../test/mplot.svg')
-        srmplt = sr_m.plot()
-        titleTime = mhd_t0 + dt.timedelta(milliseconds = m_acc_ns // 1e6)
-        srmpltTitle  = 'mplot ' + titleTime.strftime(formatString)
-        print(srmpltTitle)
-        srmplt.set_title(srmpltTitle)
-        plt.savefig(tmpPath)
-        plt.cla()
-        tmpPath.replace(realPath)
-
-    def hPlot():
-        tmpPath = Path('../test/tmp_hplot.svg')
-        realPath = Path('../test/hplot.svg')
-        srhplt = sr_h.plot()
-        srhpltTitle  = 'hplot '
-        srhpltTitle += (mhd_t0 + h_acc_ns / 1e9 ).strftime(formatString)
-        print(srhpltTitle)        
-        srhplt.set_title(srhpltTitle)
-        plt.savefig(tmpPath)
-        plt.cla()
-        tmpPath.replace(realPath)
-
-    def dPlot():
-        tmpPath = Path('../test/tmp_dplot.svg')
-        realPath = Path('../test/dplot.svg')
-        srdplt = sr_d.plot()
-        srdpltTitle  = 'dplot '
-        srdpltTitle += (mhd_t0 + d_acc_ns / 1e9 ).strftime(formatString)
-        print(srdpltTitle)        
-        srdplt.set_title(srdpltTitle)
-        plt.savefig(tmpPath)
-        plt.cla()
-        tmpPath.replace(realPath)
-
-    m_max = 16 #16 
-    h_max = 45 #45
-    d_max = 24
-    sr_m = pd.Series([0]* m_max)         # 16 * ~5s <-> ~80 s
-    sr_h = pd.Series([0]* h_max)         # 45 * ~80 s <-> ~1 h
-    sr_d = pd.Series([0]* d_max)         # 24 * ~1 h <-> ~1 day
-    m_index= 0
-    h_index = 0
-    d_index = 0
-    m_acc_ns = 0 
-    h_acc_ns = 0 
-    d_acc_ns = 0 
+    mT = MT.MhdTracker('m', 2, ['../test/','plot.svg'])      # 16 * ~5s <-> ~80 s  
+    hT = MT.MhdTracker('h', 3, ['../test/','plot.svg'])      # 45 * ~80 s <-> ~1 h 
+    dT = MT.MhdTracker('d', 4, ['../test/','plot.svg'])      # 24 * ~1 h <-> ~1 day
     print(f'{sys.argv[0]} @ {dt.datetime.now().strftime("%Y-%m-%d; %H:%M:%S")}')
     with serial.Serial('/dev/ttyACM1', 115200) as sPort:
         print('before syncOrExit')
         syncOrExit(sPort)
         print('after syncOrExit ')
-        mhd_t0 = None
         while True:
             (tns0,tns1) = struct.unpack('QQ', sPort.read(16))
-            if (not mhd_t0):
-                mhd_t0 = dt.datetime.now()
-            sr_m[m_index] = tns1 - tns0
-            m_index += 1
-            m_index %= m_max
-            if (0 == m_index):
-                m_acc_ns += sr_m.sum()
-                mPlot()
-                sr_h[h_index] = sr_m.mean()
-                doLog(h_index, sr_h[h_index])
-                h_index += 1
-                h_index %= h_max
-                if (0 == h_index):
-                    h_acc_ns += sr_h.sum()
-                    hPlot()
-                    sr_d[d_index] = sr_h.mean()
-                    d_index += 1
-                    d_index %= d_max
-                    if (0 == d_index):
-                        d_acc_ns += sr_d.sum()
-                        dPlot()
-                        d_index += 1
-                        d_index %= d_max
-
-                
+            if mT.update(tns1-tns0):
+                mT.doPlot(True)
+                if hT.update(mT.series.sum()):
+                    hT.doPlot(True)
+                    if dT.update(hT.series.sum()):
+                        dT.doPlot(True)
                 
 # if ('__main__' == __name__) :
 #     # if (2 > len(sys.argv)):
--- serial/mhdTracker.py
+++ serial/mhdTracker.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 class MhdTracker:
     """
     """
-    mhd_t0 = False
+    mhd_t0 = None
     def __init__(self, x, iMax, pathParts=['../web/', 'plot.svg'] ):
         self.x = x
         self.tmpPath = Path(f'{pathParts[0]}tmp_{x}{pathParts[1]}')
@@ -14,6 +14,7 @@ class MhdTracker:
         self.iMax=iMax
         self.i=0
         self.acc_ns=0
+        self.acc_cnt=0 
         self.series=pd.Series([0] * self.iMax)
         self.formatString = "%Y-%m-%d %H:%M:%S"
         # 2022-09-11 12:12:01
@@ -21,32 +22,34 @@ class MhdTracker:
 
         
 
-    def update(self, tns0, tns1):
+    def update(self, tnsDiff):
         if (not MhdTracker.mhd_t0):
             MhdTracker.mhd_t0 = dt.datetime.now()
-        self.series[self.i] = tns1 - tns0
+        self.series[self.i] = tnsDiff
         self.i += 1
         self.i %= self.iMax
         if (0 == self.i):
             self.acc_ns += self.series.sum()
+            self.acc_cnt += 1
         return (0 == self.i)
         
     def doPlot(self, toFile=False):
         self.plot = self.series.plot()
         self.tTime = MhdTracker.mhd_t0 + dt.timedelta(milliseconds = self.acc_ns // 1e6)
-        self.pTitle  = f'{self.x}plot ' + self.tTime.strftime(self.formatString)
+        self.pTitle  = f'{self.x}plot {self.acc_cnt % self.iMax}/({self.iMax}) @ '
+        self.pTitle += self.tTime.strftime(self.formatString)
         print(self.pTitle)
         self.plot.set_title(self.pTitle)
         if (toFile):
-            plt.savefig(tmpPath)
+            plt.savefig(self.tmpPath)
             plt.cla()
-            tmpPath.replace(realPath)
+            self.tmpPath.replace(self.realPath)
         else:
             plt.show()
 
 if '__main__' == __name__ :
     mT=MhdTracker('m', 16, pathParts=['../test/','plot.svg'])       
     for i in range(16):
-        print(mT.update(i, i*3))
+        print(mT.update(i*3-i))
     print(mT.series)
     mT.doPlot()
