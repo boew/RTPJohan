de82c2c246399a8e3014c86c4309fb89dbba2889
boe <boe@wengle.se>
2022-09-18 11:15:31 +0200

    OK: DataFrame.plot; TBD: cpxd robust sub-second td

--- serial/cpxpd.py
+++ serial/cpxpd.py
@@ -18,8 +18,9 @@ if True:
         sp.write(syncBytes)
         cr = None
         for cs in syncBytes:
-            maxCount = struct.calcsize('HHQQ')
+            maxCount = struct.calcsize('QQ') * 10
             while (bytes([cs]) != cr):
+                print(maxCount, bytes([cs]), cr)
                 cr = sp.read()
                 maxCount -= 1
                 if (0 >= maxCount):
@@ -35,8 +36,8 @@ if True:
         tmpPath = Path('../test/tmp_mplot.svg')
         realPath = Path('../test/mplot.svg')
         srmplt = sr_m.plot()
-        srmpltTitle  = 'mplot '
-        srmpltTitle += dt.datetime.now().strftime(formatString)
+        titleTime = mhd_t0 + dt.timedelta(milliseconds = m_acc_ns // 1e6)
+        srmpltTitle  = 'mplot ' + titleTime.strftime(formatString)
         print(srmpltTitle)
         srmplt.set_title(srmpltTitle)
         plt.savefig(tmpPath)
@@ -48,7 +49,7 @@ if True:
         realPath = Path('../test/hplot.svg')
         srhplt = sr_h.plot()
         srhpltTitle  = 'hplot '
-        srhpltTitle += dt.datetime.now().strftime(formatString)
+        srhpltTitle += (mhd_t0 + h_acc_ns / 1e9 ).strftime(formatString)
         print(srhpltTitle)        
         srhplt.set_title(srhpltTitle)
         plt.savefig(tmpPath)
@@ -60,7 +61,7 @@ if True:
         realPath = Path('../test/dplot.svg')
         srdplt = sr_d.plot()
         srdpltTitle  = 'dplot '
-        srdpltTitle += dt.datetime.now().strftime(formatString)
+        srdpltTitle += (mhd_t0 + d_acc_ns / 1e9 ).strftime(formatString)
         print(srdpltTitle)        
         srdplt.set_title(srdpltTitle)
         plt.savefig(tmpPath)
@@ -70,38 +71,46 @@ if True:
     m_max = 16 #16 
     h_max = 45 #45
     d_max = 24
-    sr_m = pd.Series([0.0]* m_max)         # 16 * ~5s <-> ~80 s
-    sr_h = pd.Series([0.0]* h_max)         # 45 * ~80 s <-> ~1 h
-    sr_d = pd.Series([0.0]* d_max)         # 24 * ~1 h <-> ~1 day
-    m_index=0
-    h_index=0
-    d_index=0
-    m_filled = False
+    sr_m = pd.Series([0]* m_max)         # 16 * ~5s <-> ~80 s
+    sr_h = pd.Series([0]* h_max)         # 45 * ~80 s <-> ~1 h
+    sr_d = pd.Series([0]* d_max)         # 24 * ~1 h <-> ~1 day
+    m_index= 0
+    h_index = 0
+    d_index = 0
+    m_acc_ns = 0 
+    h_acc_ns = 0 
+    d_acc_ns = 0 
     print(f'{sys.argv[0]} @ {dt.datetime.now().strftime("%Y-%m-%d; %H:%M:%S")}')
     with serial.Serial('/dev/ttyACM1', 115200) as sPort:
         print('before syncOrExit')
         syncOrExit(sPort)
         print('after syncOrExit ')
+        mhd_t0 = None
         while True:
-            r0 = sPort.read(8)
-            (tns0,) = struct.unpack('Q', r0)
-            r1 = sPort.read(8)
-            (tns1,) = struct.unpack('Q', r1)
-            td = (tns1 - tns0) // 1000000 # ns to ms
-            sr_m[m_index] = td / 1000
+            (tns0,tns1) = struct.unpack('QQ', sPort.read(16))
+            if (not mhd_t0):
+                mhd_t0 = dt.datetime.now()
+            sr_m[m_index] = tns1 - tns0
             m_index += 1
             m_index %= m_max
             if (0 == m_index):
-                sr_h[h_index] = sr_m.mean()
+                m_acc_ns += sr_m.sum()
                 mPlot()
+                sr_h[h_index] = sr_m.mean()
                 doLog(h_index, sr_h[h_index])
                 h_index += 1
                 h_index %= h_max
                 if (0 == h_index):
-                    sr_d[d_index] = sr_h.mean()
+                    h_acc_ns += sr_h.sum()
                     hPlot()
+                    sr_d[d_index] = sr_h.mean()
                     d_index += 1
                     d_index %= d_max
+                    if (0 == d_index):
+                        d_acc_ns += sr_d.sum()
+                        dPlot()
+                        d_index += 1
+                        d_index %= d_max
 
                 
                 
